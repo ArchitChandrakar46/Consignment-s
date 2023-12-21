acf1e49a16c6d194ae078444692bac59cecf2221
ArchitChandrakar46 <architchandrakar04@gmail.com>
2023-12-21 21:19:36 +0530

    First JS file

---
+++ script.js
@@ -0,0 +1,25 @@
+let a="Archit Chandrakar";
+let b=9;
+let c=True;
+let d=undefined
+let e=null;
+let f=BigInt(484894894);
+let g=Symbol(key);
+
+
+console.log(a , b, c, d, e, f, g, h);
+console.log(typeof a);
+console.log(typeof b);
+console.log(typeof d);
+console.log(typeof e);
+console.log(typeof f);
+console.log(typeof g);
+console.log(typeof h);
+
+let obj =
+{
+    name: "Jeenal Rathod",
+    age: 21, 
+    isStudent: True,
+    skils: [Gym, Business Studies, Austrailia],
+};
